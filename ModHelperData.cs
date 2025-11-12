fe17a2f01d8562b8a7baf1c6b29f246c7b610ad2
p e a c e <binh.phanpeace@hcmut.edu.vn>
2025-11-13 04:37:04 +0700

    removing unnecessary artifacts and code refactoring

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace EditPlayerData;
 
 public static class ModHelperData
 {
-    public const string Version = "1.5.6-unofficial";
+    public const string Version = "1.5.7-unofficial";
     public const string Name = "EditPlayerDataUnofficial";
 
     public const string RepoOwner = "pe4cechu";
